4eb55b082bdcba832bce11081b049e6d7ba0e894
Amit <amitaec94@gmail.com>
2020-09-20 17:27:57 +0530

    Added two methods to check the win and marker place

--- script.py
+++ script.py
@@ -1,23 +1,50 @@
 from IPython.display import clear_output
 
+
 def display_board(board):
     # print(board[7] + '|' + board[8] + '|' + board[9])
     # print(board[4] + '|' + board[5] + '|' + board[6])
     # print(board[1] + '|' + board[2] + '|' + board[3])
-    #print('   |   |')
+    # print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    #print('   |   |')
+    # print('   |   |')
     print('-------------')
-    #print('   |   |')
+    # print('   |   |')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    #print('   |   |')
+    # print('   |   |')
     print('-------------')
-    #print('   |   |')
+    # print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    #print('   |   |')
+    # print('   |   |')
+
+
+def player_input():
+    marker = ''
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1: choose X or ').upper()
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
 
 
 test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
 # test_board = [' '] * 10
+place_marker(test_board, '%', 8)
 display_board(test_board)
